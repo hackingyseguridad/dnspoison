78932649e6aac8aa01d5985e501803c9904dd838
hackingyseguridad <antonio_taboada@hotmail.es>
2020-04-20 23:20:47 +0200

    Update cache.sh

--- cache.sh
+++ cache.sh
@@ -1,3 +1,4 @@
+API_HOST=$(</dev/urandom tr -dc a-z0-9|head -c32).edns.ip-api.com; API_IP=$(dig $API_HOST +short @$1); curl -H "Host: $API_HOST" http://$API_IP/json
 dig @$1 +short porttest.dns-oarc.net TXT
 dig @$1 whoami.akamai.net +short
 dig @$1 whoami.ultradns.net +short
