d7f83acd21a2f2a0c9c6bd49d9a1d70c40583579
hackingyseguridad <antonio_taboada@hotmail.es>
2020-11-19 09:07:58 +0100

    Delete respuesta.py

--- respuesta.py
+++
@@ -1,7 +0,0 @@
-                spfResp = IP(dst=pkt[IP].src, src=pkt[IP].dst)\
-                    /UDP(dport=pkt[UDP].sport, sport=53)\
-                    /DNS(id=pkt[DNS].id, \
-                              qr=1L,\
-                              qd=DNSQR(qname=pkt[DNSQR].qname), \
-                              an=DNSRR(rrname=”trailers.apple.com”,rdata=localIP) \
-                              )
